63f5ae3f641f38bb02fc23f46268fee6ba388108
ssakthivdsr <35471150+ssakthivdsr@users.noreply.github.com>
2021-03-31 23:07:31 +0530

    expansion changes

--- src/app/expansionview/expansionview.component.ts
+++ src/app/expansionview/expansionview.component.ts
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./expansionview.component.css']
 })
 export class ExpansionviewComponent implements OnInit {
-
+  panelOpenState = false
   constructor() { }
 
   ngOnInit(): void {
