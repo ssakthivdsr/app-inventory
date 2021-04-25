7b4ae641053a61f3cf738b20d9321e79b0b31704
ssakthivdsr <35471150+ssakthivdsr@users.noreply.github.com>
2021-04-25 12:57:45 +0530

    business partner validation changes

--- src/app/component/businesspartner/businesspartner.component.ts
+++ src/app/component/businesspartner/businesspartner.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormControl, Validators } from '@angular/forms';
 import { Title } from '@angular/platform-browser';
 import { MatSnackBar } from '@angular/material/snack-bar';
+import { MatDialog, MatDialogRef } from '@angular/material/dialog';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-businesspartner',
@@ -12,47 +14,81 @@ export class BusinesspartnerComponent implements OnInit {
 
   public addBusFormGroup: FormGroup;
 
-
-
-  public constructor(private titleService: Title, private _snackBar: MatSnackBar) {
+  public constructor(private titleService: Title, private _snackBar: MatSnackBar, private dialog: MatDialog, private router: Router) {
     this.titleService.setTitle("Inventory - Business Partner Details");
     this.addBusFormGroup = new FormGroup({});
+  }
+
+  SecondaryBusinessPartner: string = '';
+  BusinessPartnerManagerNames: string = '';
+  BusinessPartnerDirectorNames: string = '';
+
+  check() {
+    if (this.SecondaryBusinessPartner === '' || this.BusinessPartnerManagerNames === '' ||
+      this.BusinessPartnerDirectorNames === '')
+      return true;
+    else
+      return false;
   }
 
   ngOnInit(): void {
     this.addBusFormGroup = new FormGroup({
       BusPart: new FormControl('', [Validators.required])
-
-
     });
   }
+
+  openDialog() {
+    this.dialog.open(DialogElementsExampleDialog);
+  }
+
   save() {
-
-    console.log("Saved");
-
+    //console.log("Saved");
+    this.openSnackBar();
   }
 
-  openSnackBar(message: string) {
-  
-    this._snackBar.open(message, "dismiss", {
+  cancel() {
+    this.router.navigate(['/landingPage']);
+  }
 
+  openSnackBar() {
+    this._snackBar.open("Details are saved successfully", "Dismiss", {
       duration: 2000,
-
       verticalPosition: "top"
-
     });
-
-
-
-    this.save();
-
   }
-
 
   public checkError = (controlName: string, errorName: string) => {
     return this.addBusFormGroup.controls[controlName].hasError(errorName);
   }
+}
 
+@Component({
+  selector: 'dialog-elements-example-dialog',
+  templateUrl: 'dialog-elements-example-dialog.html',
+})
 
+export class DialogElementsExampleDialog {
 
+  constructor(public dialogRef: MatDialogRef<DialogElementsExampleDialog>, public dialog: MatDialog, private _snackBar: MatSnackBar) { }
+
+  save() {
+    //console.log("saved");
+    this.openSnackBar();
+  }
+
+  openSnackBar() {
+    this._snackBar.open("Details are saved successfully", "Dismiss", {
+      duration: 2000,
+      verticalPosition: "top"
+    });
+  }
+
+  clickMethod() {
+    this.save();
+    this.dialogRef.close();
+  }
+
+  onNoClick(): void {
+    this.dialogRef.close();
+  }
 }
