5664b3a55f182777611970b1f220825a4dc7294b
ssakthivdsr <35471150+ssakthivdsr@users.noreply.github.com>
2021-04-28 20:48:21 +0530

    service integration changes

--- src/app/component/dept/add-dept-details/add-dept-details.component.ts
+++ src/app/component/dept/add-dept-details/add-dept-details.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { FormGroup, FormControl, Validators } from '@angular/forms';
 import { MatSnackBar } from '@angular/material/snack-bar';
+import { UserService } from '../../../service/user.service';
+import { Department } from '../../../service/user.service';
 
 @Component({
   selector: 'app-add-dept-details',
@@ -15,8 +17,9 @@ export class AddDeptDetailsComponent implements OnInit {
 
   DepartmentName: string = '';
   DepartmentOwner: string = '';
+  department : string = '';
 
-  constructor(private _snackBar: MatSnackBar, private router: Router) {
+  constructor(private _snackBar: MatSnackBar, private router: Router, private userService: UserService) {
     this.addDepFormGroup = new FormGroup({});
   }
 
@@ -25,7 +28,13 @@ export class AddDeptDetailsComponent implements OnInit {
       DpName: new FormControl('', [Validators.required]),
       DpOwner: new FormControl('', [Validators.required])
     });
+    this.userService.getProducts().subscribe((data: any) => {
+      console.log(data);
+      this.department = data
+    })
   }
+
+  
 
   save() {
     //console.log("Saved");
--- src/app/service/user.service.ts
+++ src/app/service/user.service.ts
@@ -1,6 +1,10 @@
 import { Injectable } from '@angular/core';
-import { HttpClient  } from '@angular/common/http';
+import { catchError } from 'rxjs/internal/operators';
+import { HttpClient, HttpHeaders, HttpErrorResponse } from '@angular/common/http';
+import { Observable, throwError } from 'rxjs';
+import { map } from 'rxjs/operators';
 
+const endpoint = 'https://app-inventory-restapi-test.herokuapp.com/applicationinventoryservice/retrieveDepartmentData/11';
 @Injectable({
   providedIn: 'root'
 })
@@ -12,5 +16,33 @@ export class UserService {
       return this.http.get('http://jsonplaceholder.typicode.com/users'); 
    }
 
+   private handleError(error: HttpErrorResponse): any {
+    if (error.error instanceof ErrorEvent) {
+      console.error('An error occurred:', error.error.message);
+    } else {
+      console.error(
+        `Backend returned code ${error.status}, ` +
+        `body was: ${error.error}`);
+    }
+    return throwError(
+      'Something bad happened; please try again later.');
+  }
+
+  getProducts(): Observable<any> {
+    return this.http.get<Department>(endpoint).pipe(
+      catchError(this.handleError)
+    );
+  }
+  private extractData(res: Response): any {
+    const body = res;
+    return body || { };
+  }
+
+}
+
+export interface Department {
+  id: number;  // required field
+  departmentName: string;
+  departmentOwner: String;
 }
 
