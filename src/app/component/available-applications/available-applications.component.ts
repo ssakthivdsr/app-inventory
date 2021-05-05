cae92264bbdbeaae5a48304265881aa89a0c86a7
ssakthivdsr <35471150+ssakthivdsr@users.noreply.github.com>
2021-05-05 21:20:39 +0530

    search functionality

--- src/app/component/available-applications/available-applications.component.ts
+++ src/app/component/available-applications/available-applications.component.ts
@@ -6,23 +6,33 @@ import { animate, state, style, transition, trigger } from '@angular/animations'
 
 export interface Application {
   id: number;
+  department: string;
+  lob: string;
   name: string;
   businessTotal: number;
   businessValue: number;
   agility: number;
   techTotal: number;
-  applicationCompletedStatus:String;
+  applicationCompletedStatus: string;
 }
 
 const ELEMENT_DATA: Application[] = [
-  { id: 1, name: 'Application-1', businessTotal: 86, businessValue: 20, agility: 45, techTotal: 21, applicationCompletedStatus: '70%' },
-  { id: 2, name: 'Application-2', businessTotal: 90, businessValue: 22, agility: 46, techTotal: 22, applicationCompletedStatus: '75%' },
-  { id: 3, name: 'Application-3', businessTotal: 85, businessValue: 11, agility: 43, techTotal: 31, applicationCompletedStatus: '62%' },
-  { id: 4, name: 'Application-4', businessTotal: 94, businessValue: 20, agility: 44, techTotal: 30, applicationCompletedStatus: '85%' },
-  { id: 5, name: 'Application-5', businessTotal: 80, businessValue: 15, agility: 45, techTotal: 20, applicationCompletedStatus: '78%' },
-  { id: 6, name: 'Application-6', businessTotal: 85, businessValue: 13, agility: 42, techTotal: 30, applicationCompletedStatus: '90%' },
-  { id: 7, name: 'Application-7', businessTotal: 83, businessValue: 15, agility: 45, techTotal: 23, applicationCompletedStatus: '95%' },
-  { id: 8, name: 'My-application', businessTotal: 82, businessValue: 20, agility: 42, techTotal: 20, applicationCompletedStatus: '88%' },
+  { id: 1, department:'system', lob: 'Auto and Fire Insurance', 
+  name: 'Application-1', businessTotal: 86, businessValue: 20, agility: 45, techTotal: 21, applicationCompletedStatus: '70%' },
+  { id: 2, department:'Finance', lob: 'Banking', 
+  name: 'Application-2', businessTotal: 90, businessValue: 22, agility: 46, techTotal: 22, applicationCompletedStatus: '75%' },
+  { id: 3, department:'system', lob: 'Health', 
+  name: 'Application-3', businessTotal: 85, businessValue: 11, agility: 43, techTotal: 31, applicationCompletedStatus: '62%' },
+  { id: 4, department:'system', lob: 'Life', 
+  name: 'Application-4', businessTotal: 94, businessValue: 20, agility: 44, techTotal: 30, applicationCompletedStatus: '85%' },
+  { id: 5, department:'Finance', lob: 'Billing and payments', 
+  name: 'Application-5', businessTotal: 80, businessValue: 15, agility: 45, techTotal: 20, applicationCompletedStatus: '78%' },
+  { id: 6, department:'system', lob: 'Auto and Fire Insurance', 
+  name: 'Application-6', businessTotal: 85, businessValue: 13, agility: 42, techTotal: 30, applicationCompletedStatus: '90%' },
+  { id: 7, department:'Finance', lob: 'Billing and payments', 
+  name: 'Application-7', businessTotal: 83, businessValue: 15, agility: 45, techTotal: 23, applicationCompletedStatus: '95%' },
+  { id: 8, department:'system', lob: 'Auto and Fire Insurance',
+  name: 'Application-8', businessTotal: 82, businessValue: 20, agility: 42, techTotal: 20, applicationCompletedStatus: '88%' },
 ];
 
 @Component({
@@ -40,7 +50,7 @@ const ELEMENT_DATA: Application[] = [
 
 export class AvailableApplicationsComponent implements OnInit, OnDestroy {
 
-  displayedColumns: string[] = ['id', 'name', 'Action', 'Score','applicationCompletedStatus'];
+  displayedColumns: string[] = ['id','department', 'lob',  'name', 'Action', 'Score', 'applicationCompletedStatus'];
   dataSource = new MatTableDataSource(ELEMENT_DATA);;
   _mobileQueryListener: () => void;
   isExpanded: Application | undefined;
